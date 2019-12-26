7bc762fc20a533548f18ec69812dd45bef390138
Wick Liu <wick.liu@outlook.com>
2019-12-26 11:20:26 +0800

    Variable arguments
    Resolve #2
    
    Signed-off-by: Wick Liu <wick.liu@outlook.com>

---
+++ variable_arguments.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+int add(const int num, ...)
+{
+    va_list arg;
+    va_start(arg, num);
+    int i = 0, sum = 0;
+
+    while (i < num)
+    {
+        sum += va_arg(arg, int);
+        i++;
+    }
+
+    va_end(arg);
+    return sum;
+}
+int main()
+{
+    printf("sum: %d\n", add(3, 11, 22, 33));
+    return 0;
+}
